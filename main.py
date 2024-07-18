166c8c3a0938ebd08606f39a39d437202c9cab3b
Reiner New <nbxlhc@hotmail.com>
2024-07-18 22:33:20 +0800

    add main

---
+++ main.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+author: Reiner New
+email: nbxlc@hotmail.com
+"""
+
+# from __future__ import print_function
+import ctypes
+import sys
+import getopt
+import logging.handlers
+import os
+import platform
+import time
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QSplashScreen
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication
+
+from manage import UI_CONFIG, LOGLEVEL, LOGFILE, ROOT_PATH, SettingPath, SettingFile, SETTINGS, RUNTIMEENV
+from common.config import Config
+from ui.MainWindow import MainWindow
+
+
+# from pycrunch_trace.client.api import trace
+
+
+def log(LOGLEVEL):
+    levels = [logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
+    if not os.path.exists("./logs"):
+        os.mkdir("logs")
+    LOG_FILE = LOGFILE
+    logger = logging.getLogger("myapp")
+    hdlr = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024*10240, backupCount=400, encoding='utf-8')     # 按大小进行分割
+    # hdlr = logging.handlers.TimedRotatingFileHandler(LOG_FILE, when='D', interval=1, backupCount=40)  # 按时间进行分割
+    logging.basicConfig(level=levels[LOGLEVEL],
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filename=LOG_FILE,
+                        filemode='a+')
+    logger.addHandler(hdlr)
+    logger.setLevel(LOGLEVEL)
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def suppress_keyboard_interrupt_message():
+    old_excepthook = sys.excepthook
+
+    def new_hook(exctype, value, traceback):
+        if exctype != KeyboardInterrupt:
+            old_excepthook(exctype, value, traceback)
+        else:
+            print('\nKeyboardInterrupt ...')
+            print('do something after Interrupt ...')
+
+    sys.excepthook = new_hook
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def create_file(fullPath):
+    """
+    创建日志文件夹和日志文件
+    :param filename:
+    :return:
+    """
+    (filepath, filename) = os.path.split(fullPath)
+    if not os.path.isdir(filepath):  # 无文件夹时创建
+        os.makedirs(filepath)
+    if not os.path.isfile(fullPath):  # 无文件时创建
+        fd = open(fullPath, mode="w", encoding="utf-8")
+        fd.close()
+
+def defaultConfig():
+    if not os.path.exists(os.path.join(ROOT_PATH, SettingPath)):
+        os.mkdir(SettingPath)
+
+    if not os.path.exists(os.path.join(ROOT_PATH, SettingPath, SettingFile)):
+        os.system("cp ./default/base.cfg {}".format(os.path.join(ROOT_PATH, SettingPath, SettingFile)))
+
+# @trace()
+def main(argv=None):
+    os_platform = platform.system()
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            configPath = os.path.join(ROOT_PATH, SettingPath, SettingFile)
+            create_file(configPath)
+
+            # opts, args = getopt.getopt(argv[1:], "h", ["help"])
+            log(LOGLEVEL)
+            app = QApplication(sys.argv)
+            app.setAttribute(Qt.ApplicationAttribute.AA_DontCreateNativeWidgetSiblings)
+
+            # 没有运行配置时，复制默认配置
+
+            # # config parse
+            config = Config()
+            config.setFile(configPath)
+            try:
+                config.read(SETTINGS)
+            except Exception as e:
+                print("从配置文件中未读取到正确内容", e)
+                config.init(SETTINGS)
+            except KeyError as e:
+                print("从配置文件中未读取到正确内容", e)
+                config.init(SETTINGS)
+
+            # log enable
+            log(LOGLEVEL)
+
+            # internationalization i18n setting
+            # fluentTranslator = FluentTranslator(UI_CONFIG["MainWindow"]["Language"])
+            # translator = QTranslator()
+            # translator.load(UI_CONFIG["MainWindow"]["Language"], "gallery", ".", ":/gallery/i18n")
+            #
+            # app.installTranslator(fluentTranslator)
+            # app.installTranslator(translator)
+
+            # 启动图标
+            existPic = os.path.exists(UI_CONFIG["startLogo"])
+            if existPic:
+                splash = QSplashScreen(QPixmap(UI_CONFIG["startLogo"]))
+                splash.show()
+                time.sleep(2)       # 启动图标显示2s
+
+            ui = MainWindow()
+            ui.show()
+            if existPic:
+                splash.finish(ui)
+            sys.exit(app.exec())
+
+        except getopt.error as msg:
+            raise Usage(msg)
+    except Usage as err:
+        print >> sys.stderr, err.msg
+        print >> sys.stderr, "for help use --help"
+        return 2
+
+
+
+if __name__ == "__main__":
+    # 捕获全局的 KeyboardInterrupt 异常
+    suppress_keyboard_interrupt_message()
+
+    # 判断程序运行环境: pyinstaller or pycharm
+    if RUNTIMEENV == "pyinstaller":
+        # running in a bundle
+        # # 强制以管理员权限运行
+        if is_admin():
+            # 将要运行的代码加到这里
+            sys.exit(main())
+        else:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    else:
+        # running live
+        sys.exit(main())
+
+        # if is_admin():
+        #     # 将要运行的代码加到这里
+        #     sys.exit(main())
+        # else:
+        #     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
---
+++ manage.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+author: Reiner New
+email: nbxlhc@hotmail.com
+"""
+
+import os
+import sys
+import platform
+
+# 软件版本信息
+APPNAME = "Wiretap Tunnel"
+VERSION = "v0.1.00"
+PackageTime = ""
+RUNTIMEENV = None
+
+# 0: CRITICAL, 1: ERROR, 2: WARNING, 3: INFO, 4: DEBUG
+LOGLEVEL = 4
+
+LOGFILE = "logs/app.log"
+
+os_platform = platform.system()
+maxbit = sys.maxsize
+if maxbit > 2 * 32:
+    Computer_Digits = 'x64'
+else:
+    Computer_Digits = 'x86'
+
+ROOT_PATH = os.getcwd()
+PUTTY_PATH = 'PuTTY'
+
+EncodingFormat = 'gbk'
+
+SettingPath = "./data"
+SettingFile = "setting.conf"
+
+
+UI_CONFIG = {
+    "theme": "Light",
+
+    "startLogo": "public/images/logo.png",
+    "iconPath": ":/images/icons",
+
+    "Material": {
+        "AcrylicBlurRadius": 15
+    },
+    "Update": {
+        "CheckUpdateAtStartUp": True
+    },
+    "Folders": {
+        "Download": "",
+        "LocalMusic": []
+    },
+    "MainWindow": {
+        "DpiScale": "Auto",
+        "Language": "Auto"
+    },
+    "QFluentWidgets": {
+        "ThemeColor": "#ff009faa",
+        "ThemeMode": "Dark"
+    }
+}
+
+if getattr(sys, 'frozen', False):
+    BUNDLE_DIR = sys._MEIPASS
+    RUNTIMEENV = "pyinstaller"
+    PlinkFile = os.path.join(BUNDLE_DIR, PUTTY_PATH, 'plink.exe')
+    UI_CONFIG["startLogo"] = os.path.join(BUNDLE_DIR, UI_CONFIG["startLogo"])
+else:
+    BUNDLE_DIR = os.path.dirname(os.path.abspath(__file__))
+    PlinkFile = os.path.join(PUTTY_PATH, 'plink.exe')
+
+SETTINGS = {
+    "default": {
+        "wiresharkPath": "",
+    },
+    "linux": {
+        "host": [],
+        "port": [],
+        "user": [],
+        "password": [],
+        "su_user": [],
+        "su_password": [],
+        "cmd": []
+    },
+    "esxi": {
+        "host": [],
+        "port": [],
+        "user": [],
+        "password": [],
+        "tcpdump": [],
+        "pktcap": []
+    },
+    "tcpdump": [
+
+    ],
+    "tcpdump-uw": [
+
+    ],
+    "pktcap-uw": {
+        "capture": ['Drop', 'Dynamic', 'UplinkRcv', 'UplinkSnd', 'VnicTx', 'VnicRx', 'PortInput',
+                    'IOChain', 'EtherswitchDispath', 'EtherswitchOutput', 'PortOutput',
+                    'TcpipDispatch', 'PreDVFilter', 'PostDVFilter', 'Drop', 'VdrRxLeaf',
+                    'VdrTxLeaf', 'VdrRxTerminal', 'VdrTxTerminal', 'PktFree', 'TcpipRx',
+                    'TcpipTx', 'UplinkRcvKernel', 'UplinkSndKernel', 'PreOverlayInput',
+                    'PostOverlayInput', 'PreOverlayOutput', 'PostOverlayOutput', 'EnsPortReaderRx',
+                    'EnsPortWriterTx', 'EnsPortWriterQueue', 'EnsPortWriterFlush'],
+        "outfile": [],
+        "count": [],
+        "snapLen": [],
+        "switchport": [],
+        "dir": ['0', 'input', '1', 'output', '2', 'inputAndOutput'],
+        "vlan": [],
+        "other": []
+    }
+}
